a9e0c1142313256c78f289bdcdfa103ec2e7c444
Cesar Flores <cesarflores@jalasoft.com>
2017-04-25 21:00:14 -0400

    Adding configuration params on class and on config file.

--- srv/src/main/java/datasrv/DSrvConfiguration.java
+++ srv/src/main/java/datasrv/DSrvConfiguration.java
@@ -7,4 +7,30 @@ import javax.validation.constraints.*;
 
 public class DSrvConfiguration extends Configuration {
     // TODO: implement service configuration
+
+    @NotEmpty
+    private String element;
+
+    @NotEmpty
+    private String defaultElement = "root";
+
+    @JsonProperty
+    public String getElement() {
+        return element;
+    }
+
+    @JsonProperty
+    public void setElement(String element) {
+        this.element = element;
+    }
+
+    @JsonProperty
+    public String getDefaultElement() {
+        return defaultElement;
+    }
+
+    @JsonProperty
+    public void setDefaultElement(String defElem) {
+        this.defaultElement = defElem;
+    }
 }
